72bdd6f37650487df8bc10d9a7ea8e1e4d77b523
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2023-11-14 20:26:55 +0900

    Create JAVA라이브러리.java

---
+++ JAVA/JAVA라이브러리.java
@@ -0,0 +1,4 @@
+라이브러리(.jar) - 개발 시 활용 가능한 클래스와 인터페이스들을 모아놓은 것.
+만드는 법 - 프로젝트 우클릭 - Export 클릭 - Java - JAR file 클릭 - 원하는 프로젝트 및 클래스, 경로 지정해주고 옵션 정하고 finish
+
+프로젝트 우클릭 - Build Path - Configuration Build Path 클릭 - Libraries - Add External JARs 클릭 - 추가
