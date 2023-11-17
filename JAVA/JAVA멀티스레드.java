a66510dbc927caef4ea2588f1a6284990743d6e3
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2023-11-17 11:09:37 +0900

    Update JAVA멀티스레드.java

--- JAVA/JAVA멀티스레드.java
+++ JAVA/JAVA멀티스레드.java
@@ -3,5 +3,14 @@ ex) 게임, 클라이언트-서버
 스레드 : 코드의 실행 흐름.
 ---------------------------------------------------------------------------------------------
 모든 자바 프로그램은 메인스레드가 main()메소드를 실행하면서 시작. -> main()메소드의 마지막코드 or return 만나면 실행 종료.
-자바는 스레드또한 객체로 관리 - Thread 객체명 = Thread.currentThread(); System.out.println(currThread.getName());
+자바는 작업 스레드를 객체로 관리 - Thread 객체명 = Thread.currentThread(); System.out.println(currThread.getName());
 멀티스레드에서는 실행중인 스레드가 존재 시 프로세스 종료 X.
+---------------------------------------------------------------------------------------------
+Thread클래스로 작업 스레드 객체 직접 생성 - Thread 객체명 = new Thread(Runnable 구현객체명); 객체명.start(); //Runnable upcasting
+class 구현클명 implements Runnable {
+  @Override
+  public void run() {
+    //스레드가 실행할 코드
+  }}
+Runnable 객체명A =  new 구현클명(); //upcasting
+Thread thread = new Thread(객체명A);
