f643c8acdca7bfd62ba403fe75085bfa2c750b5e
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2023-12-20 20:09:36 +0900

    Update JAVA스트림.java

--- JAVA/JAVA스트림.java
+++ JAVA/JAVA스트림.java
@@ -6,12 +6,16 @@
 Stream<T> stream = 컬렉션객체명.stream(); - Stream을 한번 이용하면 재사용 불가능 -> 다시 stream()을 통해 받아야 함.
 stream.forEach((item)->{ 처리 내용 });
 
+forEach문에 대해.
 forEach(Consumer<? super T> action)
 -> Consumer이 FunctionalInterface임.
 -> void accept(T t) 함수를 구현하는 것.
 -> 컬렉션객체에 들어있는 자료형과 Stream<>의 자료형이 동일하기 때문에 T 또한 동일.
 -> 따라서 stream의 각 요소를 item으로 넘겨 처리.
 
-병렬 처리를 하는법 -> 요소 적을 땐 not good.
+병렬 처리를 하는법 - 논리 프로세서 수만큼 병렬 Thread를 만들어 처리
+       순서 상관 X / 요소 적을땐 not good.
 Stream<T> stream = 컬렉션객체명.parallelStream();
 stream.forEach((item) -> { 처리 내용 })
+---------------------------------------------------------------------------------------------------------------
+중간 처리 및 최종 처리
