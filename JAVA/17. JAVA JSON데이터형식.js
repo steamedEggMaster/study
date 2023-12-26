01163f4d650b2269d7d4c7d1db366ac8abe721e5
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2023-12-26 19:55:57 +0900

    Update 17. JAVA JSON데이터형식.js

--- JAVA/17. JAVA JSON데이터형식.js
+++ JAVA/17. JAVA JSON데이터형식.js
@@ -1,16 +1,13 @@
 JSON(JavaScript Object Notation) - 네트워크 통신에서 가장 많이 사용되는 데이터 형식
+https://github.com/stleary/JSON-java -> click here if you just want the latest release jar file
 
 객체 표기
 {
-  "속성명":속성값,
-  "속성명":속성값,
-  ...
-}
-속성명 - 반드시 "" 로 감싸야함!!
-속성값으로 가능한것 
-- "문자열", 숫자, true/false
-- 객체 { ... } //객체 표기법 고대로 사용
-- 배열 [ ... ] //배열 표기법 고대로 사용
+  "속성명":속성값,                  속성명 - 반드시 "" 로 감싸야함!!
+  "속성명":속성값,                  속성값으로 가능한것 
+  ...                              - "문자열", 숫자, true/false
+}                                  - 객체 { ... } //객체 표기법 고대로 사용
+                                   - 배열 [ ... ] //배열 표기법 고대로 사용
 
 배열 표기
 [ 항목, 항목, ... ]
@@ -18,3 +15,4 @@ JSON(JavaScript Object Notation) - 네트워크 통신에서 가장 많이 사�
 - "문자열", 숫자, true/false
 - 객체 { ... }
 - 배열 [ ... ]
+
