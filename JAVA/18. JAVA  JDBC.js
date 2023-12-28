30d6cc39ff026eaab2752f0607f356bd5961c426
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2023-12-28 21:34:31 +0900

    Update 18. JAVA  JDBC.js

--- JAVA/18. JAVA  JDBC.js
+++ JAVA/18. JAVA  JDBC.js
@@ -17,3 +17,19 @@ sqlplus / as sysdba -> alter profile default limit password_life_time unlimited;
 원격 연결
 1. Oracle-OraDB19Home1 -> Net Configuration Assistant 클릭 -> 재구성 및 다음 -> 다음 및 예 -> 다음 -> 다음 -> 아니오 및 다음 -> 완료
 해당 포트번호 1521 방화벽 해제 - 16번 참고
+------------------------------------------------------------------------------------------
+DB 연결
+JDBC Driver 및 
+1. DBMS가 설치된 컴퓨터의 IP주소
+2. DBMS가 허용하는 포트 번호
+3. 사용자(DB계정) 및 비밀번호
+4. 사용하고자 하는 DB 이름
+
+Oracle은 다운 시 jdbc\lib\ojdbc8.jar 이라는 JDBC Driver 파일이 이미 있음.
+1. java에서 Build Path 시켜주기
+
+2. 클라이언트 프로그램을 DB와 연결하기 위해 가장 먼저 해야 할 일 : JDBC Driver를 메모리로 로딩!
+: Class.forName("JDBC Driver클래스") - oracle은 "oracle.jdbc.OracleDriver"
+-> DriverManager에 JDBC Driver 객체를 등록하게 됨. JDBC Driver 클래스를 찾지 못하면 "ClassNotFoundException" 발생
+
+3. Connection conn = DriverManager.getConnection("연결 문자열", "사용자", "비밀번호"
