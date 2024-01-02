5304e8ac148705ee76d1cc2147f134d433e3350b
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-02 17:05:51 +0900

    Update SpringBoot.js

--- SpringBoot/SpringBoot.js
+++ SpringBoot/SpringBoot.js
@@ -111,9 +111,11 @@ View - 데이터를 보여주는 화면 영역 (UI 요소들) / 별도의 데이
                 - 클래스와 메소드의 RequestMapping을 통해 URL을 매핑하여 경로를 설정하여 해당 메서드에서 처리  
                 - 고전 방법이라 사용 X
 ------------------------------------------------------------------------
-Get API
-1. @GetMapping(value = "/~")
-2. @PathVariable - get형식의 요청에서 매개변수(파라미터)를 전달하기 위해 URL에 값을 담아 요청하는 방법
+Get API : 서버에 있는 리소스를 가져오기 위해 사용하는 API
+
+@GetMapping(value = "/~")
+- 파라미터 받는 방식
+1. @PathVariable - get형식의 요청에서 매개변수(파라미터)를 전달하기 위해 URL에 값을 담아 요청하는 방법
    방법
     -1. 파라미터의 이름 == value에서 사용된 변수이름
       @GetMapping(value = "/variable1/{variable}")
@@ -121,17 +123,24 @@ Get API
     -2. 파라미터의 이름 != value에서 사용된 변수이름
       @GetMapping(value = "/variable2/{variable}")
       접근제어자 반환값 함수명(@PathVariable("variable") 타입 변수명) { ~ }
-3. @RequestParam - get형식의 요청에서 쿼리 문자열을 전달하기 위해 사용되는 방법
-4. DTO - get형식의 요청에서 쿼리 문자열을 전달하기 위해 사용되는 방법으로 별도의 객체를 생성하여 받는 방식
+2. @RequestParam - get형식의 요청에서 쿼리 문자열을 전달하기 위해 사용되는 방법
+3. DTO - get형식의 요청에서 쿼리 문자열을 전달하기 위해 사용되는 방법으로 별도의 객체를 생성하여 받는 방식
+
+GetController 예제 잘 보기
 ------------------------------------------------------------------------
-Post API - 리소스를 추가하기 위해 사용되는 API
-@PostMapping : POST API를 제작하기 위해 사용되는 어노테이션(annotation) / @RequestMapping + POST method의 조합
-@RequestBody를 사용하여 Body에 담겨있는 값을 받아야함. - Talend API에서 JSON으로 작성함.
+Post API : 리소스를 추가하기 위해 사용되는 API
+         - @PostMappingPOST API를 제작하기 위해 사용되는 어노테이션(annotation)
+         - @RequestMapping + POST method의 조합
+         - @RequestBody를 사용하여 Body에 담겨있는 값을 받아야함. - Talend API에서 JSON으로 작성함.
 { //간략한 JSON 예시
   "name" : "Flature",
   "email" : "cksdud5014@naver.com",
   "organization" : "cksdud-Studio"
 }
+@PostMapping(value = "~")
+- 파라미터 받는 방식
+1. @RequestBody Map<~,~>
+2. @RequestBody DTO
 ------------------------------------------------------------------------
 기존에는 서버 개발자가 변경될 때마다 문서를 만들어 프론트엔드 개발자에게 보내줘야 했음.
 Swagger 라이브러리 - 서버로 요청되는 API 리스트를 HTML 화면으로 문서화하여 테스트 할 수 있는 라이브러리
