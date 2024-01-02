6a830c0b8751c1b0bc82275ad95070f1a0e7dbfb
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-02 20:15:14 +0900

    Update StringBoot 팁.js

--- SpringBoot/StringBoot 팁.js
+++ SpringBoot/StringBoot 팁.js
@@ -1,1 +1,11 @@
+**alt + insert 키** : 자동 getter setter등 만들어 줌
 
+Talend API
+
+해당 클래스 위에서 ctrl + 좌클릭
+
+400단위 : 클라에러
+500단위 : 서버에러
+
+화면에 toString이 아닌 JSON형태로 반환되는 것은 Controller의 return 타입이 DTO 객체이기 때문. String이면 toString()으로 작성한 내용이 나옴.
+PutController의 DTO객체를 이용한 방법 3가지 자세히 볼것.
