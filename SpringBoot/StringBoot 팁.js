73579bc92e50ea715f3e5e81576f4ac60de873d9
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-12 12:55:24 +0900

    Update StringBoot 팁.js

--- SpringBoot/StringBoot 팁.js
+++ SpringBoot/StringBoot 팁.js
@@ -7,7 +7,8 @@ Talend API
 400단위 : 클라에러
 500단위 : 서버에러
 
-화면에 toString이 아닌 JSON형태로 반환되는 것은 Controller의 return 타입이 DTO 객체이기 때문. String이면 toString()으로 작성한 내용이 나옴.
+화면에 toString이 아닌 JSON형태로 반환되는 것은 Controller의 return 타입이 
+(DTO 객체 and ResponseEntity) 이기 때문. String이면 toString()으로 작성한 내용이 나옴.
 PutController의 DTO객체를 이용한 방법 3가지 자세히 볼것.
 
 port번호 변경 방법
