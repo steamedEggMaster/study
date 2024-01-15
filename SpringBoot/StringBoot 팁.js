e95f8f0483de135836ba0e8a1192439fcb044e01
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-15 17:46:10 +0900

    Update StringBoot 팁.js

--- SpringBoot/StringBoot 팁.js
+++ SpringBoot/StringBoot 팁.js
@@ -14,3 +14,6 @@ PutController의 DTO객체를 이용한 방법 3가지 자세히 볼것.
 port번호 변경 방법
 1. application.properties 파일에서 server.port=원하는 포트번호
 2. Run -> Edit Configurations -> Environment variables에서 server.port=원하는 포트번호
+
+MariaDB에 @Table 어노테이션을 통해 Entity로 테이블 생성 시
+Entity의 필드가 대문자를 경계로 "~_~"(모두 소문자)로 컬럼이름이 정해짐.
