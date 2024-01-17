5b514d81cdc6bc1cf75aa506b59400d09755c691
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-17 17:04:15 +0900

    Create Interceptor.js

---
+++ SpringBoot/Interceptor.js
@@ -0,0 +1,39 @@
+인터셉터(Interceptor)
+: DispatcherServlet과 컨트롤러(Controller) 사이에서 요청(Request)를 가로채는 역할 수행
+- 인터셉터 사용 시 기존 로직 수정 없이 비지니스 로직 전후에서 특정 기능 수행 가능
+- 인터셉터 구현을 위해 "HandlerInterceptor" 사용
+
+----- HandlerInterceptor 구조
+          --------      -----------        ----------------
+          |      | ---> |          | <---> | HandlerMapping |                                ↓ 위부터 아래로 순서
+          |      |      |          |        ----------------                                 ↓
+          |      |      |          |        -------------                                    ↓
+          |      |      |          |       |             |        |---------------------|    ↓
+Request   |      |      |          | ----> | Handler     | <----> | Handler(Controller) |    ↓
+--------> |      |      |Dispatcher|       | Interceptor |        |---------------------|    ↓
+          |Filter|      | Servlet  | <---- |             |                  ↑                ↓
+<-------- |      |      |          |        -------------                   ↓                ↓
+Response  |      |      |          |                                    | Logic |            ↓
+          |      |      |          | <---> | ViewResolver|                                   ↓
+          |      | <--- |          | <---> | View |                                          ↓
+          --------      -----------    
+Filter와 HandlerInterceptor는 비슷
+차이점은 DispatcherServlet 우측의 내용들은 Spring Context에 존재하는데 Filter는 그 바깥에 있어 Spring Component에 접근하기 어렵다
+                                                                    HandlerInterceptor는 Compoent에 접근이 쉽다
+
+HandlerInterceptor 인터페이스
+public interface HandlerInterceptor {
+    default boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) Exception { }
+
+    default void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
+      @Nullable ModelAndView modelAndView) throws Exception {  }
+
+    default void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, 
+      @Nullable Exception ex) throws Exception {  }
+}
+default 메서드는 인터페이스의 기본 메서드로 구현클래스에서 반드시 구현할 필요가 없음 - 필요한 것만 하면됨
+1. preHandle 메서드 : 컨트롤러로 Request가 가기 전에 수행할 코드를 작성하는 메서드
+                    - return 값이 true -> 컨트롤러로 Request 전달, false -> 전달 X
+                    - Object handler : 요청을 전달할 컨트롤러 객체가 담겨있음
+2. postHandle 메서드 : 컨트롤러 로직이 수행된 후 "View가 렌더링 되기 전"에 수행할 코드를 작성하는 메서드
+3. afterCompletion 메서드 : "View가 렌더링 된 후"에 실행되는 메서드
