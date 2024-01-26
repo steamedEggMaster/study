f0acd412e9730af351f8eb84e7e9f3f71c5e58e5
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-26 16:25:58 +0900

    Update 1.js

--- Linux/1.js
+++ Linux/1.js
@@ -1,7 +1,7 @@
 마우스 빼는 법 : ctrl + art
 
 게스트OS와 호스트OS 간 데이터 교환하는 방법
-1. freeISOCreaㅅer 설치
+1. freeISOCreater 설치
 2. 교류할 파일 선택
 3. iso파일 생성
 4. vmware player의 edit setting 에서 CD/DVD -> ISO 파일 찾기 해서 넣어주면 됨
@@ -15,3 +15,6 @@ id : rocky
 pw : rocky
 
 pro와 player 동시 실행 불가 
+
+한영 변환 키 : shift + space
+cap 키 : win + space
