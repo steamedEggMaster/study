cc8726d64b3edc22f0ed8b5e3d90f771bd04121f
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-01-31 16:23:41 +0900

    Update 4. 기본키 생성 전략.js

--- SpringBoot/JPA/4. 기본키 생성 전략.js
+++ SpringBoot/JPA/4. 기본키 생성 전략.js
@@ -4,29 +4,52 @@
 - 기본 키를 자동으로 생성하는 방식에서는 @GeneratedValue 어노테이션을 통해 전략 설정 가능
 
 전략 설정
-1. 직접 할당 방식 : 기본키를 애플리케이션에서 자체적으로 생성하여 할당하는 방식
-                 - @GeneratedValue 어노테이션 사용 안하고 직접 값 주입
-                 - 이 방식은 애플리케이션에서 별도의 키 생성 규칙을 정의하여 사용해야 중복 문제 발생 X
+1. (Direct) 직접 할당 방식 : 기본키를 애플리케이션에서 자체적으로 생성하여 할당하는 방식
+                          - @GeneratedValue 어노테이션 사용 안하고 직접 값 주입
+                          - 이 방식은 애플리케이션에서 별도의 키 생성 규칙을 정의하여 사용해야 중복 문제 발생 X
+                          - 
+
 
 2. Identity
-- @GeneratedValue(strategy = GenerationType.IDENTITY)
+-   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
 - 기본키 생성을 DB에 위임하는 방식
 - 컬럼에 AUTO_INCREMENT 기능 활용하여 값을 사용
 - Identity 전략은 persist() 메서드가 호출되는 시점에 Insert 쿼리가 실행되며 식별자를 조회 -> 그래야 엔티티에서 쓰는 넘버가 뭔지 알 수 있음
                   - 일반적으로는 commit() 시점에 쿼리 실행
 
+
+
 3. Sequence
-- @GeneratedValue(strategy = GenerationType.SEQUENCE)
+- @Entity                              
+  @SequenceGenerator( // Sequence 방식을 사용하기 위해선 @SequenceGenerator 어노테이션을 아래와 같이 적어줘야 함
+    name = "sequence_generator",     // name : 시퀀스 생성기의 이름을 지정. 이 이름은 @GeneratedValue 에서 사용됨
+    sequenceName = "my_seq",         // sequenceName : 식별자(Id)를 생성할 때 사용할 Sequence 이름을 지정
+    allocationSize = 1)              // initialValue : DDL을 통해 DB를 생성할 때 사용되는 값으로, 최초 설정되는 값을 정의
+  @Table(name = "sequence")          // allocationSize : 시퀀스에서 읽어온 값을 기준으로 몇 개의 식별자를 생성할 지 결정. 값을 1로 설정
+  public class SequenceEntity { }
+
+- @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
 - DB의 Sequence를 이용하는 방식
 - DB의 Sequence : Unique 값을 생성해주는 객체
                 - 일반적으로 Primary Key를 생성하기 위해 사용됨
                 - 지원하는 DB : Oracle, PostgresSQL, H2, Maria DB 등
                 - 대체로 Oracle 에서만 사용
-- Sequence 방식을 사용하기 위해선 @SequenceGenerator 어노테이션을 아래와 같이 적어줘야 함
-ex) @SequenceGenerator(
-     name = "sequence_generator",
-     sequenceName = "my_seq",
-     allocationSize = 1)
+  
+-----  allocationSize를 설정하지 않을 경우 아래와 같은 방식으로 식별자 생성
+* 1. 최초에 DB 시퀀스에서 값을 구함. 이 값을 식별자 구간의 시작 값으로 사용
+* 2. 한번 더 DB 시퀀스에 값을 구함. 이를 식별자 구간의 끝 값으로 사용
+* 3. 구간의 시작 값부터 끝 값까지 순차적으로 엔티티의 식별 값으로 사용
+       이때 메모리에 순차적으로 증가한 값을 보관.
+       --> 이 과정 때문에 allocationSize를 1로 설정하지 않았을 경우, 여러 JVM이 가동되는 가정하에 충돌이 발생하게 됨
+* 4. 구간의 끝에 다다르면 DB 시퀀스에 값을 구함
+       시퀀스에서 구한 값을 다음 식별자 구간의 끝 값으로 사용한다. 구간 끝 값에서 (allocationSize -1)를 뺀 값을 식별자 구간의 시작 값으로 사용
+* 5. 과정 3과 4를 반복
+
 
 4. Table
 - @GeneratedValue(strategy = GenerationType.TABLE)
