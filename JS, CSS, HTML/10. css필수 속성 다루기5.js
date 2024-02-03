bd1872dd06feaf7eec5b1967e7852e8cdb41d4ae
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-03 21:49:19 +0900

    Update and rename 10. css 필수 속성 다루기 to 10. css필수 속성 다루기5.js

---
+++ JS, CSS, HTML/10. css필수 속성 다루기5.js
@@ -0,0 +1,8 @@
+----- 웹 폰트와 아이콘 폰트
+웹 폰트 : 인증된 기관 or 회사에서 웹 서버에 올려 놓은 포트 ex) 구글 폰트 (https://fonts.google.com)
+
+아이콘 폰트 : 다양한 아이콘을 이미지 파일이 아닌 CSS를 적용하는 방식
+           - 사용법
+             1. https://cdnjs.com/libraries/font-awesome에서 폰트어썸의 최신버전의 주소를 copy 한 후
+                <link> 태그의 href 속성에 붙여넣기
+             2. https://fontawesome.com/icons 사이트에서 free 인것들 중 하나를 골라 HTML을 그대로 복사하여 붙여넣기
