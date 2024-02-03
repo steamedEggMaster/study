d5525e4383b322de89ba8de97b78a768fb887b3f
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-03 21:14:25 +0900

    Update 15. Docker.js

--- SpringBoot/15. Docker.js
+++ SpringBoot/15. Docker.js
@@ -5,3 +5,10 @@ image 실행 : docker run -p 3306:3306 --name mariadb -e MARIADB_ROOT_PASSWORD=a
                                  (interactive 모드 및 terminal 연결)
 실행중인 컨테이너 내에서 명령 실행 : docker exec -it mariadb /bin/bash
                                             (실행중인컨테이너 이름)       /bin/bash : bash쉘 실행명령
+
+1. mysql 설치 : docket pull mysql:원하는버전
+2. container 생성 : docker run -p 3306:3306 --name 이름지정 -e MYSQL_ROOT_PASSWORD=패스워드 -d mysql:버전
+3. 해당 container안에서 bash shell 실행 : docker exec it 컨테이름 bash
+4. bash shell에서 mysql을 실행하는 방법
+   - 1. mysql -u root -p
+   - 2. 패스워드 입력
