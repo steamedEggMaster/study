6ded790a52d1e34368f2b1fb9885d2b69917eccd
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-11 12:15:34 +0900

    Update StringBoot 팁.js

--- SpringBoot/StringBoot 팁.js
+++ SpringBoot/StringBoot 팁.js
@@ -11,9 +11,14 @@ Talend API
 (DTO 객체 and ResponseEntity) 이기 때문. String이면 toString()으로 작성한 내용이 나옴.
 PutController의 DTO객체를 이용한 방법 3가지 자세히 볼것.
 
-port번호 변경 방법
+----- port번호 변경 방법
 1. application.properties 파일에서 server.port=원하는 포트번호
 2. Run -> Edit Configurations -> Environment variables에서 server.port=원하는 포트번호
 
 MariaDB에 @Table 어노테이션을 통해 Entity로 테이블 생성 시
 Entity의 필드가 대문자를 경계로 "~_~"(모두 소문자)로 컬럼이름이 정해짐.
+
+----- redirect
+return "redirect:/경로"
+      - redirect: 라는 접두사를 사용
+      - 어떤 경로에 대한 요청을 redirect 하여 설정 경로로 보내줌.
