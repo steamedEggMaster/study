78d7abc782b7d7ec5b6c4b1f499a1102d9e1d2d7
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-11 13:48:37 +0900

    Update 0. SpringBoot 기본.js

--- SpringBoot/0. SpringBoot 기본.js
+++ SpringBoot/0. SpringBoot 기본.js
@@ -2,7 +2,9 @@
                           - 이 파일에서 @SpringBootApplication 어노테이션을 통해 애플리케이션을 구성하고 실행
 2. application.properties : 애플리케이션의 전반적 설정 저장 파일.
                        - 필요한 경우 application.yml 로 변경 가능
-3. static : 정적 리소스(HTML, CSS, JavaScript 등)를 저장하는 디렉토리. 웹 브라우저에 의해 직접 요청되는 파일 저장
+3. static : 정적 리소스(HTML, CSS, JavaScript 등)를 저장하는 디렉토리. 웹 브라우저에 의해 직접 지정하여 요청되는 파일 저장
+          - ex) http://localhost:8080/hello.html
+
 4. template : 타임리프(Thymeleaf) 와 같은 서버 사이드 템플릿 엔진이 사용하는 뷰 템플릿 파일들을 저장하는 디렉토리
 5. 프로젝트명ApplicationTests.java : 애플리케이션의 테스트 코드 작성 파일.
                                   - 스프링부트 프로젝트는 기본적으로 JUnit 테스트 프레임워크 사용
@@ -19,3 +21,7 @@
 
 ----- 내장서버 
 - 기본적으로 내장 톰캣 사용, 필요한 경우 Jetty, Undertow 등으로 변경 가능
+
+----- form data(폼 데이터)
+: HTML 요소인 <form> 태그에 실려 전송되는 데이터
+- 웹 브라우저에서 서버로 데이터 전송 시 사용
