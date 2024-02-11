4344e69d8cd40e45f21b329e69123304071a3b71
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-11 18:43:33 +0900

    Update 6. SpringBoot Rest Template.js

--- SpringBoot/aroundhub/6. SpringBoot Rest Template.js
+++ SpringBoot/aroundhub/6. SpringBoot Rest Template.js
@@ -22,10 +22,22 @@ Method                                                          HTTP       설�
 - uri에 @PathVariable, @RequestParam에 해당하는 파라미터 지정 가능
 - Object에 @Requestbody에 해당하는 객체 넣기 가능
 
-@PathVariable - url에 변수가 있을때 사용
-              - UriComponentsBuilder().fromUriString(~).path(~).encode().build().expand(~)로 사용 - 복수 개 일시 , 으로 구분
-@RequestParam - '경로끝?파라미터명=~'로 보낼 시 사용
-              - Uri 생성 시 UriComponentsBuilder().fromUriString(~).path(~).queryParam(name, value)로 사용 - 경로 가장끝에 표현해야해서
+@PathVariable - url에 변수가 있을때 사용                                              *
+              - UriComponentsBuilder().fromUriString(~).path(~).encode().build().expand(~).encode().build().toUri();로 사용 - 복수 개 일시 , 으로 구분
+@RequestParam - '경로끝?파라미터명=~'로 보낼 시 사용                               *  
+              - Uri 생성 시 UriComponentsBuilder().fromUriString(~).path(~).queryParam(name, value).encode().build().toUri();로 사용 - 경로 가장끝에 표현해야해서
+@@RequestBody는 restTemplate을 통해 보낼 때 설정                                                     //encode() - UTF-8 로 Encoding
+
+----- Http 헤더에 무언가를 추가해야할 때
+HttpHeaders headers = new HttpHeaders();
+headers.setAccept(Arrays.asList(new MediaType[]{MediaType.APPLICATION_JSON}));
+headers.setContentType(MediaType.APPLICATION_JSON);
+headers.set("X-Naver-Client-Id", clientId);
+headers.set("X-Naver-Client-Secret", clientSecret);
+
+HttpEntity<String> entity = new HttpEntity<>("", headers);
+
+----- 
 
 ----------------------------------------------------------------------------------------------
 WebClient 
