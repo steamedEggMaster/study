9ac4604c673ddc72d717679a0fe1800f27cebce7
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-12 19:08:01 +0900

    Update 3. CustomOAuth2UserService.js

--- SpringBoot/OAuth2/3. CustomOAuth2UserService.js
+++ SpringBoot/OAuth2/3. CustomOAuth2UserService.js
@@ -7,3 +7,16 @@ SecurityConfig 와 소셜 로그인 서비스 API 신청 후 CustomOAuth2UserSer
 2. loadUser() 메서드 @Overriding
    코드 - @Override
           public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException
+
+3. super.loadUser(userRequest) 를 통해 OAuth2User 객체 생성하기
+   코드 - OAuth2User oAuth2User = super.loadUser(userRequest);
+   - 받은 json 을 oAuth2User.getAttributes() 로 확인 가능
+                 // Map<String, Object> return
+4. 외부 소셜 로그인 서비스 업체의 이름이 담긴 registrationId 값을 받기 위해 아래의 코드 사용
+   코드 - String registrationId = userRequest.getClientRegistration().getRegistrationId();
+                                              //클라이언트 등록정보    // 등록 ID
+5. registrationId 를 비교하여 그에 맞는 json을 받을 ResponseDto 생성
+   코드 -  if(registrationId.equals("naver")){ oAuth2Response = new NaverResponse(oAuth2User.getAttributes()); }
+           else if (registrationId.equals("google")){ oAuth2Response = new GoogleResponse(oAuth2User.getAttributes()); }
+           else { return null; }
+6. 
