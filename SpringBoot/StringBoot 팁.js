bb1b60e7afd2af5385d9818fa401c13c93047211
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-13 17:29:01 +0900

    Update StringBoot 팁.js

--- SpringBoot/StringBoot 팁.js
+++ SpringBoot/StringBoot 팁.js
@@ -23,8 +23,18 @@ return "redirect:/경로"
       - redirect: 라는 접두사를 사용
       - 어떤 경로에 대한 요청을 redirect 하여 설정 경로로 보내줌.
 
-1. 웹페이지와 서버간에 데이터가 왔다갔다 할때는 @RestController
+----- @RestController 와 @Controller 차이
+   웹페이지와 서버간에 데이터가 왔다갔다 할때는 @RestController
    MVC에서 변수를 받아 뷰 템플릿에 보여주기 위해 사용하는 컨트롤러는 @Controller
 
-2. template 에 한국어 ??? 로 나타날 때
+----- template 에 한국어 ??? 로 나타날 때
    application.properties 에서 server.servlet.encoding.force=true
+
+----- 인텔리제이의 콘솔에 출력할때 한글이 깨진다면 파일 인코딩 설정을 UTF-8 로 변경
+1. File -> Setting -> Editor > File Encodings 클릭
+   -> Global Encoding 및 Project Encoding, Default encoding for properties files
+      이 3가지를 전부 UTF-8 로 설정 후
+      apply -> ok
+2. Help -> Edit Custom VM Options 클릭
+   -> idea64.exe.vmoptions 편집기 열림 -> 맨아래에 -Dfile.encoding=UTF-8 추가
+   -> 닫고, intellij 종료 후 재실행
