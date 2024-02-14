7d73c10898e09e2651372f355b605fc25b7effc6
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-14 14:41:42 +0900

    Update 5. update.js

--- SpringBoot/홍팍/5. update.js
+++ SpringBoot/홍팍/5. update.js
@@ -1,4 +1,4 @@
-Form 태그는 옛날 태그라 Patch(put) 기능 지원 X
+Form 태그는 옛날 태그라 Patch(put) 기능 지원 X - Post 와 Get 만 지원
 -> PostMapping으로 수행.
 
 - DB에 내용을 갱신할 때는 해당되는 객체가 있다면
@@ -11,6 +11,11 @@ Form 태그는 옛날 태그라 Patch(put) 기능 지원 X
          INSERT INTO article(id, title, content) VALUES (2, '나나나나', '2222');
          INSERT INTO article(id, title, content) VALUES (3, '다다다다', '3333');
          * H2 Database 는 sql 문법을 사용! *
+           -- 간략한 sql
+              1. create : INSERT INTO 테이블명(컬럼,,,) VALUES (갑,,,);
+              2. read : SELECT 컬럼,,, FROM 테이블명 WHERE 조건;
+              3. update : UPDATE 테이블명 SET 컬럼 = 값 WHERE 조건;
+              4. delete
 
       2. properties 파일에
          spring.jpa.defer-datasource-initialization=true
