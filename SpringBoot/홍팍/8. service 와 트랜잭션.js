b5ffe7cee08afdcda99a54ea2bcd1b1a6967d983
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-14 19:57:01 +0900

    Update 8. service 와 트랜잭션.js

--- SpringBoot/홍팍/8. service 와 트랜잭션.js
+++ SpringBoot/홍팍/8. service 와 트랜잭션.js
@@ -11,3 +11,10 @@ return (조건문) ? true 내용 : false 내용;
 
 ----- orElseThrow(() -> new 예외클래스("띄울말"));
 ex) articleRepository.findById(-1L).orElseThrow(() -> new IllegalArgumentException("결제 실패!"));
+
+
+----- 트랜잭션
+트랜잭션은 보통 Service 에서 관리
+- 간단한 사용법
+  : service의 메서드에 @Transactional 어노테이션 붙이기
+                      - 예외 발생 시 롤백
