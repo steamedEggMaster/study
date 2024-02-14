be8f606f1d9f3c357906a2baa1297c5360c30f7b
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-14 14:36:47 +0900

    Create 5. update.js

---
+++ SpringBoot/홍팍/5. update.js
@@ -0,0 +1,17 @@
+Form 태그는 옛날 태그라 Patch(put) 기능 지원 X
+-> PostMapping으로 수행.
+
+- DB에 내용을 갱신할 때는 해당되는 객체가 있다면
+  save() 로 갱신 가능
+
+----- 더미데이터
+: 재실행할때마다 데이터를 넣기 귀찮으니 실행될때마다 저장소에 넣어주는 것.
+- ex) 1. data.sql 파일을 resource에 생성
+         INSERT INTO article(id, title, content) VALUES (1, '가가가가', '1111');
+         INSERT INTO article(id, title, content) VALUES (2, '나나나나', '2222');
+         INSERT INTO article(id, title, content) VALUES (3, '다다다다', '3333');
+         * H2 Database 는 sql 문법을 사용! *
+
+      2. properties 파일에
+         spring.jpa.defer-datasource-initialization=true
+         설정
