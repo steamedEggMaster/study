3a667b98aece3619e6d97a642ff0270391aa77e7
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-14 16:07:04 +0900

    Update 5. update.js

--- SpringBoot/홍팍/5. update.js
+++ SpringBoot/홍팍/5. update.js
@@ -16,6 +16,10 @@ Form 태그는 옛날 태그라 Patch(put) 기능 지원 X - Post 와 Get 만 �
               2. read : SELECT 컬럼,,, FROM 테이블명 WHERE 조건;
               3. update : UPDATE 테이블명 SET 컬럼 = 값 WHERE 조건;
               4. delete : DELETE FROM 테이블명 WHERE 조건;
+              5. 테이블 생성 : CREATE TABLE 테이블명 (
+                                 속성명1 자료형,
+                                  ...
+                                 PRIMARY KEY ( 기본키속성명) );
 
       2. properties 파일에
          spring.jpa.defer-datasource-initialization=true
