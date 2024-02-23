7ba9ac239e4d77c1b81326933ad52c854c04c984
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-23 22:56:22 +0900

    Update and rename 3. 회원가입.js to 3. 회원가입, 회원 중복 검증.js

--- SpringBoot/security/3. 회원가입, 회원 중복 검증.js
+++ SpringBoot/security/3. 회원가입, 회원 중복 검증.js
@@ -20,3 +20,9 @@
 2. 각각 다 만들어주기
 3. SecurityConfig의 requestMatchers() 에 join 및 joinProc 경로 추가하기
 ---------------------------------------------------------------------------------
+1. UserEntity의 Username은 Unique 해야하기 때문에
+   @Column(unique = true) 설정
+
+2. userRepository에서 커스텀 메서드 만들기
+3. JoinService에서 검사하기
+
