d11163b76039708d963a13c5eb0280820cc2a651
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-23 18:05:58 +0900

    Create 6. (중급) Number, Math.js

---
+++ JS, CSS, HTML/JAVASCRIPT/6. (중급) Number, Math.js
@@ -0,0 +1,8 @@
+- toString([진수]) : 진수에 맞는 숫자로 변환 후 String 화
+  ex) let num = 10;
+      num.toString() -> "10"
+      num.toString(2) -> "1010"
+
+----- Math
+1. Math.PI; : 3.14~ return 
+2. Math.ceil(수 | 수로된변수) 
