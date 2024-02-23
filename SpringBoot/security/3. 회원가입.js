c0fa96982078e4f2addc724373a02c5fbcba55ea
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-23 22:54:09 +0900

    Update and rename 3. 회원가입 로직.js to 3. 회원가입.js

--- SpringBoot/security/3. 회원가입.js
+++ SpringBoot/security/3. 회원가입.js
@@ -14,3 +14,9 @@
                    |  | Controller |    |   Service  |                          |
                    |  ◣-----------◢    ◣-----------◢                          |
                    ◣-----------------------------------------------------------◢
+
+
+1. Join Controller, Join Service, JoinDto, UserEntity, UserRepository 를 생성하기
+2. 각각 다 만들어주기
+3. SecurityConfig의 requestMatchers() 에 join 및 joinProc 경로 추가하기
+---------------------------------------------------------------------------------
