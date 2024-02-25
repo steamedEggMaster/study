8b3eb88839bb628b19096aea483beb6f64343a22
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-25 19:51:18 +0900

    Update 9. (중급) setTimeout, setInterval, call, apply, bind.js

--- JS, CSS, HTML/JAVASCRIPT/9. (중급) setTimeout, setInterval, call, apply, bind.js
+++ JS, CSS, HTML/JAVASCRIPT/9. (중급) setTimeout, setInterval, call, apply, bind.js
@@ -14,3 +14,23 @@ clearInterval(tId) : 반복 함수 실행 중단
 
 밀리세컨드 = 0 -> 바로 실행 X
                  why? 브라우저는 기본 4ms 지연시간 있고, 해당 스크립트가 끝나야 실행됨
+---------------------------------------------------------------------------------------------------
+call, apply, bind
+: 함수 호출 방식과 관계없이 this 지정 가능
+
+ex) const mike = { name : 'Mike' }
+    const tom = { name : 'Tom' }
+    function showThisName() { console.log(this.name); }
+    일때, this 는 window 를 가리킴
+
+1. 함수명.call(this로 사용할객체[, 인수1, 인수2,...]);         
+    ex) showThisName.call(mike); -> this 는 mike 객체를 가리킴
+
+2. 함수명.apply(this로 사용할객체[, [인수1, 인수2, ...]]); - 배열로 넣어주기
+    ex) function update(birthYear, occupation){ this.birthYear = birthYear; this.occupation = occupation; }
+    일때,
+    update.apply(mike, [2001, "engineer"]);
+
+3. const new함수명 = this객체변경할함수명.bind(this로 사용할객체) : this 가 가리키는 객체를 영구히 바꿈
+    ex) const updateMike = update.bind(mike);
+        updateMike(1980, "doctor");
