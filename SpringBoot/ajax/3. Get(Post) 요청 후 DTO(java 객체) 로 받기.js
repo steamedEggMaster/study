f2e2d90250891603b5495e220200483d80a4bdd3
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-26 16:14:05 +0900

    Update 3. Get(Post) 요청 후 DTO(java 객체) 로 받기.js

--- SpringBoot/ajax/3. Get(Post) 요청 후 DTO(java 객체) 로 받기.js
+++ SpringBoot/ajax/3. Get(Post) 요청 후 DTO(java 객체) 로 받기.js
@@ -1,9 +1,13 @@
+-- GET 요청
 동일하게 템플릿을 작성하고
 컨트롤러에서
-
 @GetMapping("/ex05")
 public @ResponseBody AjaxDto ex05(@ModelAttribute AjaxDto ajaxDto){
     System.out.println("ajaxDto = " + ajaxDto);
     return ajaxDto; }
 와 같이 @ResponseBody에 객체를 담아 return 할때,
+
  *** res 에는 Java 객체 -> JS 객체로 변환되어 들어옴. ***
+
+-- POST 요청
+도 동일.
