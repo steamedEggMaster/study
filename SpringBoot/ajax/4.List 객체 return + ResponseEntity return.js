79c8780a7b39822733411570a8d85631010c9509
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-02-26 17:05:10 +0900

    Update 4.List 객체 return + ResponseEntity return.js

--- SpringBoot/ajax/4.List 객체 return + ResponseEntity return.js
+++ SpringBoot/ajax/4.List 객체 return + ResponseEntity return.js
@@ -3,3 +3,8 @@
  -> res는 JS객체 리스트를 받음.
     그후 사용은 js문법에 따라서.
 -------------------------------------------------------------------
+-- ResponseEntity 의 HttpStatus 를 통해 ajax "성공 여부 컨트롤" 가능
+ex) @PostMapping("/ex09")
+    public ResponseEntity ex09(@RequestBody AjaxDto ajaxDto){
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND); }
+    -> ajax 가 실패하여 error: 프로퍼티 실행됨.
